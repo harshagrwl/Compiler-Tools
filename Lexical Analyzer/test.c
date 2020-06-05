4455aa84c42043764112cb8dc6d50aeef60629da
Harsh Agrawal <agrawal.harsh14@gmail.com>
2020-06-05 23:25:07 +0530

    Added Lexical Analyzer Code

---
+++ Lexical Analyzer/test.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+void test()
+{
+printf("Testing");
+}
+
+//Single line comment
+
+/*
+Multi line comment
+\)(\:)? {if( !isComment) printf("\n"); ECHO;}
+\( ECHO;
+*/
+
+int main()
+{
+int _a=0x1, b=08 ,b=(1);
+int bye;
+int hi;
+int x=a+b;
+printf("Hello World");
+x=pow(a,b);
+for(int a=0;;)
+printf("%d",x);
+}
